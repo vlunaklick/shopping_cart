875554d45ce9edd174b7503e054dd5494b933b91
Valen <v.lunaklick@gmail.com>
2021-06-30 00:19:35 -0300

    Home done

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,7 +5,20 @@ export default function HomeShow(){
     return(
         <>
             <main>
-                
+                <div className="containerHome">
+                    <div className="imgHome">
+                        <div>
+                            <div className="textHome">
+                                <h3 className="h3H">Make your dream computer come true.</h3>
+                                <h3 className="h3H">Be a real gamer.</h3>
+                            </div>
+                            <button className="bShop">SHOP NOW</button>
+                        </div>
+                    </div>
+                    <div className="imgHome">
+                        <img src="https://i.imgur.com/msGgRDM.png" alt="" />
+                    </div>
+                </div>  
             </main>
         </>
     ) 
