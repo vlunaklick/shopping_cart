2094abbe82a6b49a3325cd2bd5e60c283be87b82
Valen <v.lunaklick@gmail.com>
2021-06-30 22:57:18 -0300

    Better things

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,11 +1,25 @@
 import './styles.css';
+import TargetCheck from '../../components/TargetCheck'
+import { useState } from 'react';
 
 
-export default function Checkout(){
+export default function Checkout(props){
+
+    let [totalP,changeTot] = useState(0)
+
     return( 
         <>
             <main className="mainShop">
+                <div className="total">
+                    <div className="izqPanel">
+                        <h4 className="titNavIzq">TOTAL</h4>
+                        <h2 className="totS">${totalP}</h2>
+                    </div>
 
+                    <div className="derPanel">
+                        <TargetCheck />
+                    </div>
+                </div>
             </main>
         </>
     ) 
