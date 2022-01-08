2d69d5c2ab202f2e894debb668a4ff2c937918fa
Valen <v.lunaklick@gmail.com>
2022-01-08 14:04:03 -0300

    change size goUp

--- src/components/GoUp/index.js
+++ src/components/GoUp/index.js
@@ -15,8 +15,8 @@ function GoUp({ show }) {
 				<svg
 					className='goup-arrow'
 					xmlns='http://www.w3.org/2000/svg'
-					width='24'
-					height='24'
+					width='16'
+					height='16'
 					viewBox='0 0 24 24'>
 					<path d='M24 12l-10-10v7h-14v6h14v7z' />
 				</svg>
