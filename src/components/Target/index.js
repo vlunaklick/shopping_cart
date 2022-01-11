0367dc465f5a0d8c400ef8c3f6d4b7fd33e94a5a
Valen <v.lunaklick@gmail.com>
2022-01-11 14:40:13 -0300

    Target in styled-components

--- src/components/Target/index.js
+++ src/components/Target/index.js
@@ -1,61 +1,142 @@
-import './style.css'
+import styled from 'styled-components'
 
-export default function Target(props) {
-    return (
-        <>
-            <section className="target">
-                <div className="target-top-part">
-                    <img className="target-top-part-img" src={props.image} alt="" />
-                </div>
-                <div className="target-bottom-part">
-                    <div className="target-bottom-part-text">
-                        <h5 className="target-bottom-part-text-name">{props.nameU}</h5>
-                        <p className="target-bottom-part-text-price">${props.price}</p>
-                    </div>
-                    <div className="target-bottom-part-btn">
-                        <div
-                            className="target-bottom-part-btn-left"
-                            onClick={() => props.restar(props.nameU)}
-                        >
-                            <svg
-                                aria-hidden="true"
-                                focusable="false"
-                                data-prefix="fas"
-                                data-icon="minus"
-                                className="svg-inline--fa fa-minus fa-w-14 svgBtn"
-                                role="img"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 448 512"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-                                ></path>
-                            </svg>
-                        </div>
-                        <div
-                            className="target-bottom-part-btn-rigth"
-                            onClick={() => props.sumar(props.nameU)}
-                        >
-                            <svg
-                                aria-hidden="true"
-                                focusable="false"
-                                data-prefix="fas"
-                                data-icon="plus"
-                                className="svg-inline--fa fa-plus fa-w-14 svgBtn"
-                                role="img"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 448 512"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-                                ></path>
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+function TargetL(props) {
+	return (
+		<>
+			<section className={props.className}>
+				<div className='target-top-part'>
+					<img src={props.image} alt='' />
+				</div>
+				<div className='target-bottom-part'>
+					<div className='target-bottom-part-text'>
+						<h5>{props.nameU}</h5>
+						<p>${props.price}</p>
+					</div>
+					<div className='target-bottom-part-btn'>
+						<div
+							className='target-bottom-part-btn-left'
+							onClick={() => props.restar(props.nameU)}>
+							<svg
+								aria-hidden='true'
+								focusable='false'
+								data-prefix='fas'
+								data-icon='minus'
+								className='svg-inline--fa fa-minus fa-w-14 svgBtn'
+								role='img'
+								xmlns='http://www.w3.org/2000/svg'
+								viewBox='0 0 448 512'>
+								<path
+									fill='currentColor'
+									d='M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z'></path>
+							</svg>
+						</div>
+						<div
+							className='target-bottom-part-btn-rigth'
+							onClick={() => props.sumar(props.nameU)}>
+							<svg
+								aria-hidden='true'
+								focusable='false'
+								data-prefix='fas'
+								data-icon='plus'
+								className='svg-inline--fa fa-plus fa-w-14 svgBtn'
+								role='img'
+								xmlns='http://www.w3.org/2000/svg'
+								viewBox='0 0 448 512'>
+								<path
+									fill='currentColor'
+									d='M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z'></path>
+							</svg>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	)
 }
+
+const Target = styled(TargetL)`
+	filter: drop-shadow(0px 0.4px 0.5px hsl(210deg 12% 60% / 0.34));
+
+	.target-top-part {
+		width: 17.5rem;
+		height: 17.5rem;
+		background-color: #cbd5e1;
+		border-top-left-radius: 0.313rem;
+		border-top-right-radius: 0.313rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	img {
+		width: 90%;
+		height: 90%;
+		object-fit: cover;
+		transition: 0.5s;
+	}
+	h5,
+	p {
+		color: #f8fafc;
+		font-weight: 400;
+		line-height: 1rem;
+	}
+	h5 {
+		font-size: 1rem;
+		font-weight: 600;
+	}
+	p {
+		font-size: 0.625rem;
+	}
+	.target-bottom-part {
+		width: 17.5rem;
+		height: 4.25rem;
+		background-color: #94a3b8;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-bottom-left-radius: 0.313rem;
+		border-bottom-right-radius: 0.313rem;
+	}
+	.target-bottom-part-text {
+		display: flex;
+		flex-direction: column;
+		margin-left: 0.5rem;
+		width: 100%;
+	}
+	.target-bottom-part-btn {
+		display: flex;
+		justify-content: center;
+		width: 80%;
+	}
+	.target-bottom-part-btn-left {
+		width: 2.875rem;
+		height: 1.813rem;
+		border-bottom-left-radius: 0.313rem;
+		border-top-left-radius: 0.313rem;
+		background-color: #f1f5f9;
+		cursor: pointer;
+	}
+	.target-bottom-part-btn-rigth {
+		width: 2.875rem;
+		height: 1.813rem;
+		border-bottom-right-radius: 0.313rem;
+		border-top-right-radius: 0.313rem;
+		background-color: #f1f5f9;
+		cursor: pointer;
+	}
+	.target-bottom-part-btn-rigth,
+	.target-bottom-part-btn-left {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		transition: 0.2s;
+	}
+	.target-bottom-part-btn-rigth:hover,
+	.target-bottom-part-btn-left:hover {
+		background-color: rgb(195, 210, 223);
+	}
+	svg {
+		width: 0.875rem;
+	}
+`
+
+export default Target
