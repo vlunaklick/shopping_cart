b9306bc1e3117b6424e11ac05a7836ee9af69204
Valen <v.lunaklick@gmail.com>
2022-01-11 13:48:18 -0300

    Navigator using styled-components

--- src/components/Navigator/index.js
+++ src/components/Navigator/index.js
@@ -1,43 +1,131 @@
 import { Link } from 'react-router-dom'
-import './nav.css'
+import styled from 'styled-components'
 
 function NavigatorL(props) {
 	return (
-		<nav className='navigator'>
-			<Link
-				to='/shopping_cart/'
-				className='title-nav'
-				onClick={() => props.cerrar()}>
-				Gaming Store
-			</Link>
-			<ul className='list-links-nav'>
-				<li className='list-li-nav'>
-					<Link
-						to='/shop/all'
-						className='list-hiper-nav'
-						onClick={() => props.cerrar()}>
-						SHOP
-					</Link>
-				</li>
+		<>
+			<nav className={props.className}>
+				<Link
+					id='title-link'
+					to='/shopping_cart/'
+					onClick={() => props.cerrar()}>
+					Gaming Store
+				</Link>
+				<ul>
+					<li>
+						<Link
+							id='list-hiper-nav'
+							to='/shop/all'
+							onClick={() => props.cerrar()}>
+							SHOP
+						</Link>
+					</li>
 
-				<li className='list-li-nav gap-cart-nav'>
-					<span className='list-hiper-nav' onClick={() => props.carrito()}>
-						<svg
-							width='25'
-							height='25'
-							viewBox='0 0 36 36'
-							xmlns='http://www.w3.org/2000/svg'>
-							<path
-								d='M25.815 13.5L19.245 3.66C18.96 3.24 18.48 3.03 18 3.03C17.52 3.03 17.04 3.24 16.755 3.675L10.185 13.5H3C2.175 13.5 1.5 14.175 1.5 15C1.5 15.135 1.515 15.27 1.56 15.405L5.37 29.31C5.715 30.57 6.87 31.5 8.25 31.5H27.75C29.13 31.5 30.285 30.57 30.645 29.31L34.455 15.405L34.5 15C34.5 14.175 33.825 13.5 33 13.5H25.815ZM13.5 13.5L18 6.9L22.5 13.5H13.5ZM18 25.5C16.35 25.5 15 24.15 15 22.5C15 20.85 16.35 19.5 18 19.5C19.65 19.5 21 20.85 21 22.5C21 24.15 19.65 25.5 18 25.5Z'
-								fill='#F0F4F8'
-							/>
-						</svg>
-					</span>
-					<p className='cart-amount-nav'>{props.cant}</p>
-				</li>
-			</ul>
-		</nav>
+					<li id='gap-cart-nav'>
+						<span onClick={() => props.carrito()}>
+							<svg
+								width='25'
+								height='25'
+								viewBox='0 0 36 36'
+								xmlns='http://www.w3.org/2000/svg'>
+								<path
+									d='M25.815 13.5L19.245 3.66C18.96 3.24 18.48 3.03 18 3.03C17.52 3.03 17.04 3.24 16.755 3.675L10.185 13.5H3C2.175 13.5 1.5 14.175 1.5 15C1.5 15.135 1.515 15.27 1.56 15.405L5.37 29.31C5.715 30.57 6.87 31.5 8.25 31.5H27.75C29.13 31.5 30.285 30.57 30.645 29.31L34.455 15.405L34.5 15C34.5 14.175 33.825 13.5 33 13.5H25.815ZM13.5 13.5L18 6.9L22.5 13.5H13.5ZM18 25.5C16.35 25.5 15 24.15 15 22.5C15 20.85 16.35 19.5 18 19.5C19.65 19.5 21 20.85 21 22.5C21 24.15 19.65 25.5 18 25.5Z'
+									fill='#F0F4F8'
+								/>
+							</svg>
+						</span>
+						<p>{props.cant}</p>
+					</li>
+				</ul>
+			</nav>
+		</>
 	)
 }
+
+const Navigator = styled(NavigatorL)`
+	display: flex;
+	padding: 0.4rem 0.72657rem;
+	background-color: #90a7bb;
+	z-index: 3;
+	position: sticky;
+	top: 0;
+
+	li {
+		list-style: none;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	ul {
+		display: flex;
+		align-items: center;
+		width: 100%;
+		padding: 0;
+		justify-content: right;
+		gap: 0.5rem;
+		transform: translate(-10%);
+	}
+
+	span,
+	#list-hiper-nav {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 1.8rem;
+		padding: 0.5rem;
+		transition: 0.2s;
+		font-size: 0.75rem;
+		cursor: pointer;
+		color: #fff;
+	}
+
+	span:hover,
+	#list-hiper-nav:hover {
+		background-color: #0000003d;
+	}
+
+	#gap-cart-nav {
+		display: flex;
+		align-items: center;
+		gap: 0.1rem;
+	}
+
+	#title-link {
+		font-size: 1.5rem;
+		width: 100%;
+		color: #fff;
+		font-weight: 700;
+		display: flex;
+		align-items: center;
+	}
+
+	p {
+		background-color: #64748b;
+		border-radius: 1.875rem;
+		color: #fff;
+		font-size: 0.7rem;
+		padding: 0.2rem 0.4rem;
+	}
+
+	@media (max-width: 740px) {
+		position: sticky;
+		width: 100%;
+		padding: 0.25rem 0;
+
+		li {
+			text-align: center;
+		}
+
+		#title-link {
+			text-align: center;
+			font-size: 1.2rem;
+			transform: translate(10%);
+		}
+		#list-hiper-nav {
+			font-size: 0.75rem;
+		}
+	}
+`
 
 export default Navigator
