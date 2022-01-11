16c8153eb6a4f745b7ee07d0b1b07ffba5e45a59
Valen <v.lunaklick@gmail.com>
2022-01-11 15:44:38 -0300

    404 Error in styled-components

--- src/pages/404 Error/index.js
+++ src/pages/404 Error/index.js
@@ -1,12 +1,37 @@
-import './error.css'
+import styled from 'styled-components'
 
-export default function Error404() {
-    return (
-        <>
-            <main className="main-error">
-                <h1 className="main-error-title">Error 404</h1>
-                <p className="main-error-paragraph">This page has not been found</p>
-            </main>
-        </>
-    )
+function Error404S({ className }) {
+	return (
+		<>
+			<main className={className}>
+				<h1>Error 404</h1>
+				<p>This page has not been found</p>
+			</main>
+		</>
+	)
 }
+
+const Error404 = styled(Error404S)`
+	background-color: #f1f5f9;
+	min-height: calc(100vh - 5.813rem);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+
+	h1,
+	p {
+		color: #000;
+	}
+
+	h1 {
+		font-size: 4rem;
+	}
+
+	p {
+		font-size: 2rem;
+		text-align: center;
+	}
+`
+
+export default Error404
